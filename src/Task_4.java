fa646aa89c09d95e01ab7cbeaaeae7b7f09e96b7
tzotzo77 <dpisnenco@mail.ru>
2020-04-08 11:32:31 +0300

    задание 5

--- src/Task_4.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Task_4 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Введите число в двоичном формате");
-        int n = sc.nextInt();
-        System.out.println("Число =  " + n);
-    }
-}
