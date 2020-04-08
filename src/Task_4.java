82e1feffbd0ff3ad048fc3ab10b72aedb74abea6
tzotzo77 <dpisnenco@mail.ru>
2020-04-08 10:49:34 +0300

    Тест Commit And Push

---
+++ src/Task_4.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Task_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число в двоичном формате");
+        int n = sc.nextInt();
+        System.out.println("Число =  " + n);
+    }
+}
