e857e73e15f5952319509e5c6a3a75f20f873760
tzotzo77 <dpisnenco@mail.ru>
2020-04-08 11:43:37 +0300

    test

--- src/Task_5.java
+++ src/Task_5.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Task_5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Введите число в диапазоне от -2147483648 до 2147483647 : ");
+        System.out.println("Введите число в диапазоне от -2147483648 до 2147483647: ");
         int n = sc.nextInt();
         System.out.println("1 * " + n + " = " + 1 * n);
         System.out.println("2 * " + n + " = " + 2 * n);
