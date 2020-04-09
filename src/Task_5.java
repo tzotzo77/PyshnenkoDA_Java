0b486bc67ed02b088a09dac454838d68475b141b
tzotzo77 <dpisnenco@mail.ru>
2020-04-09 09:28:51 +0300

    Задание 5 int заменен на double

--- src/Task_5.java
+++ src/Task_5.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Task_5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Введите число в диапазоне от -2147483648 до 2147483647: ");
-        int n = sc.nextInt();
+        System.out.println("Введите число : ");
+        double n = sc.nextDouble();
         System.out.println("1 * " + n + " = " + 1 * n);
         System.out.println("2 * " + n + " = " + 2 * n);
         System.out.println("3 * " + n + " = " + 3 * n);
